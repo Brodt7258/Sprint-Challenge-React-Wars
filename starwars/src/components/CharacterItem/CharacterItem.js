a5af1e40e6d534c27a4f7f14453533f24cd50d66
Tim Dill <brodt7258@gmail.com>
2019-05-18 14:32:50 -0400

    display further character details and add styling

--- starwars/src/components/CharacterItem/CharacterItem.js
+++ starwars/src/components/CharacterItem/CharacterItem.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
+import './CharacterItem.css';
+
 const CharacterItem = ({ character }) => {
   return (
-    <li>{character.name}</li>
+    <li className="char-item">
+      <h2>{character.name}</h2>
+      <ul className="char-details">
+        <li><strong>birth year:</strong> {character.birth_year}</li>
+        <li><strong>gender:</strong> {character.gender}</li>
+        <li><strong>height:</strong> {character.height} cm</li>
+        <li><strong>mass:</strong> {character.mass} kg</li>
+        <li><strong>eye color:</strong> {character.eye_color}</li>
+        <li><strong>hair color:</strong> {character.hair_color}</li>
+        <li><strong>skin color:</strong> {character.skin_color}</li>
+      </ul>
+    </li>
   );
 };
 
--- starwars/src/components/ContentList/ContentList.js
+++ starwars/src/components/ContentList/ContentList.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 import CharacterItem from '../CharacterItem/CharacterItem';
 
+import './ContentList.css';
+
 const ClassList = ({ characters }) => {
   return (
-    <ul>
+    <ul className="content-list">
       {characters.map(e => (
         <CharacterItem character={e} key={e.name} />
       ))}
